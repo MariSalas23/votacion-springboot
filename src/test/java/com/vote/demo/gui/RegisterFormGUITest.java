d21914e81aa89076139413bb8a6115218bf0470a
MariSalas23 <mari.masagu@gmail.com>
2025-05-13 16:24:39 -0500

    Update

--- src/test/java/com/vote/demo/gui/RegisterFormGUITest.java
+++ src/test/java/com/vote/demo/gui/RegisterFormGUITest.java
@@ -30,6 +30,7 @@ public class RegisterFormGUITest {
     @Test
     void testRegistroExitoso() {
         String baseUrl = System.getenv().getOrDefault("VOTEAPP_URL", "http://localhost:8080/");
+        
         driver.get(baseUrl);
         driver.findElement(By.name("id")).sendKeys(String.valueOf(System.currentTimeMillis()));
         driver.findElement(By.name("name")).sendKeys("Ana Prueba");
